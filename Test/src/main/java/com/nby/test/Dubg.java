85f5ab0577ea09b8f36461df1965fe0bd9aa407f
NiuBoYuan <103012703+niuboyuan@users.noreply.github.com>
2022-04-05 15:04:18 +0800

    提交

---
+++ Test/src/main/java/com/nby/test/Dubg.java
@@ -0,0 +1,13 @@
+package com.nby.test;
+
+/**
+ * @program: 1tEST
+ * @description: 测试类
+ * @author: NBY
+ * @create: 2022-04-05 15:01
+ **/
+public class Dubg {
+    public static void main(String[] args) {
+
+    }
+}
